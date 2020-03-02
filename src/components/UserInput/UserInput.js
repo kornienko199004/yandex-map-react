998297c8e32bcfdc74b48b3fb6d3b8127e4f97e4
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2020-03-02 12:25:25 +0300

    Add a state, add a point's array, add an output of points

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 const userInput = props => {
+  const points = props.points.map(
+    ({ name, id }) => {
+      return (
+        <li key={id}>{name}</li>
+      )
+    }
+  );
   return (
-    <div className="col-md-6 col-lg-6">
-      <p>User input</p>
-      <input type="text" placeholder="Новая точка"/>
-    </div>
+    <Aux>
+      <div className="col-md-6 col-lg-6">
+        <p>User input</p>
+        <input type="text"
+          value={props.value}
+          placeholder="Новая точка"
+          onChange={props.onChange}
+          onKeyPress={props.keyPress} />
+
+          <ul className="col-md-12 col-lg-12">
+            {points}
+          </ul>
+      </div>
+    </Aux>
   )
 };
 
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import UserInput from '../../components/UserInput/UserInput';
 import Map from '../../components/Map/Map';
 
+const DEFAULT_COORDS = [55.76, 37.64];
+
 class Layout extends Component {
+  state = {
+    inputValue: '',
+    points: [
+      // { name: string; coords: number[], id: number }
+    ]
+  }
+
+  inputHandler = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
+  inputKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.setState((state, props) => ({
+        points: [...state.points,
+          { name: state.inputValue, coords: DEFAULT_COORDS, id: Date.now() }
+        ],
+        inputValue: '',
+      }));
+    }
+  }
+
   render() {
     return (
       <div className="container row text-center col-md-12">
-        <UserInput />
+        <UserInput
+          value={this.state.inputValue}
+          onChange={this.inputHandler}
+          keyPress={this.inputKeyPress}
+          points={this.state.points}
+          />
         <Map />
       </div>
-    )
+    );
   }
 }
 
